85daebe1825a83d36ced07d1a0067f6abac98419
Allen, Scott <MAllen@tcsg.edu>
2023-12-19 17:31:36 -0500

    upgrades

--- draper.sh
+++ draper.sh
@@ -31,6 +31,7 @@ do
         "Option 1: Google Maps Scrape")
             echo "You chose Google Maps Scrape"
             cp -p "./src/google-maps-scrape.sh" ./work/${project_name}/google-maps-scrape.sh
+            cp -p "./src/env.sh" ./work/${project_name}/env.sh
             bash "./work/${project_name}/google-maps-scrape.sh" "${project_name}" "${fuid}"
             ;;
         "Option 2: Facebook Scrape")
