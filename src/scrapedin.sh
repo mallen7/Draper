76ba76dcacd451cda68f19f7aaa78282529d6d5c
Allen, Scott <MAllen@tcsg.edu>
2023-12-22 21:51:24 -0500

    upgrades

--- src/scrapedin.sh
+++ src/scrapedin.sh
@@ -15,6 +15,8 @@ read -rp "Enter target region: " region
 
 # Run the LinkedIn scraper
 echo "Running LinkedIn scraper..."
+pwd
 cd "work/${proj_name}/"
 pwd
 python3 scrapedin.py -i "${industry}" -g "${region}" -o "work/linkedin_scrape_${pid}.csv"
+pwd
